d6883e588af01064139f1e39dae81dc7e54c4faa
Kyle Husmann <kyle.husmann@gmail.com>
2019-04-29 20:47:40 -0400

    added buttons and logo

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hi Meg!</Text>
+        <Text style={styles.titleText}>Pennsylvania Butterflies</Text>
+        <Image 
+          source={require('./assets/logo.png')}
+        />
+        <Button
+          style={styles.menuButton}
+          title="Butterflies by Group"
+        />
+        <Button
+          style={styles.menuButton}
+          title="All Butterflies"
+        />
+        <Button
+          style={styles.menuButton}
+          title="What butterflies are in this app?"
+        />
+        <Button
+          style={styles.menuButton}
+          title="About"
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  titleText: {
+    color: 'white',
+    fontSize: 30,
+  },
+  menuButton: {
+
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
 });
