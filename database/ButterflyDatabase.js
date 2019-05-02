3a6116c370607d1927d25231ef5e774a179ebab3
Kyle Husmann <kyle.husmann@gmail.com>
2019-05-02 12:32:24 -0400

    single file bfdb

--- database/ButterflyDatabase.js
+++ database/ButterflyDatabase.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import { View, Text } from 'react-native';
 
-import imagelookup from './imagelookup';
-
-const BFDB = require('./bfdb').map(i => ({
-  ...i,
-  image: imagelookup[i.image],
-}));
-
-export default BFDB;
+    const imagelookup = {'blues/104c.jpg': require('../assets/butterflies/blues/104c.jpg'),'blues/103c.jpg': require('../assets/butterflies/blues/103c.jpg')};
+    const bfdb_raw = [{"nobs":"19","name":"Spring Azure","sciName":"Celastrina 'ladon'","family":"Lycaenidae","subfamily":"Polyommatinae","group":"Blues","image":"blues/104c.jpg","copyright":"\"Spring Azure\" by asdfsdf","extra":""},{"nobs":"100","name":"Spring Azure2","sciName":"Celastrina 'ladon'","family":"Lycaenidae","subfamily":"Polyommatinae","group":"Blues","image":"blues/103c.jpg","copyright":"\"Spring Azure\" by asdfsdf","extra":""}];
+    const bfdb = bfdb_raw.map(i => ({
+      ...i,
+      image: imagelookup[i.image],
+    }));
+    export default bfdb;
+  
--- database/updatedb.js
+++ database/updatedb.js
@@ -4,19 +4,22 @@ import fs from 'fs';
 import path from 'path';
 
 const url = 'http://docs.google.com/spreadsheets/d/1BX7s2M5uOrcX0cgZmR7CnWcxh7U2YkRWE16d7TkwH5I/gviz/tq?tqx=out:csv&sheet=Butterflies';
-const outFile = 'bfdb.json';
-const imageLoaderScriptFile = 'imagelookup.js';
+const outFile = 'ButterflyDatabase.js';
 
 request(url, { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   const bfdb = parse(body, { columns: true });
-  const imageLoaderScript = `export default {${bfdb.map(i => `'${i.image}': require('../assets/butterflies/${i.image}')`)}}`;
-  fs.writeFile(path.resolve(__dirname, outFile), JSON.stringify(bfdb), err => {
+  const bfdbString = `
+    const imagelookup = {${bfdb.map(i => `'${i.image}': require('../assets/butterflies/${i.image}')`)}};
+    const bfdb_raw = ${JSON.stringify(bfdb)};
+    const bfdb = bfdb_raw.map(i => ({
+      ...i,
+      image: imagelookup[i.image],
+    }));
+    export default bfdb;
+  `;
+  fs.writeFile(path.resolve(__dirname, outFile), bfdbString, err => {
     if (err) { return console.log(err); }
     console.log('Updated butterflies!');
-  });
-  fs.writeFile(path.resolve(__dirname, imageLoaderScriptFile), imageLoaderScript, err => {
-    if (err) { return console.log(err); }
-    console.log('Updated image loader script!');
   });
 });
