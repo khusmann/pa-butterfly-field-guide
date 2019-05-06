94f2ec64c5435e8337875188c4024dabd94126c6
Kyle Husmann <kyle.husmann@gmail.com>
2019-05-06 15:10:43 -0400

    black background on butterflyscreen

--- screens/ButterflyScreen.js
+++ screens/ButterflyScreen.js
@@ -10,13 +10,16 @@ export default ButterflyScreen = (props) => {
   const butterfly = BFDB.find((item) => item.name == navigation.getParam('butterfly'));
   const { width, height } = Image.resolveAssetSource(butterfly.image);
   return (
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: '#000' }}>
         <FullWidthImage source={butterfly.image} />
-        <Text>
+        <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
           Scientific Name: <Text style={{ fontStyle: 'italic' }}>{butterfly.sciName}</Text>
         </Text>
-        <Text>
+        <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
           Subfamily: <Text style={{ fontStyle: 'italic' }}>{butterfly.subfamily}</Text>
+        </Text>
+        <Text style={{ fontSize: 12, margin: 10, marginTop: 30, color: '#fff' }}>
+          {butterfly.copyright}
         </Text>
       </View>
   );
