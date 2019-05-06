95e7788867a5aabb9bca0b1b2f45d8003a72a2e7
Kyle Husmann <kyle.husmann@gmail.com>
2019-05-06 16:13:23 -0400

    convert to node-fetch from request

--- database/updatedb.js
+++ database/updatedb.js
@@ -1,14 +1,16 @@
-import request from 'request';
+import fetch from 'node-fetch';
 import parse from 'csv-parse/lib/sync';
 import fs from 'fs';
 import path from 'path';
 
-const url = 'http://docs.google.com/spreadsheets/d/1BX7s2M5uOrcX0cgZmR7CnWcxh7U2YkRWE16d7TkwH5I/gviz/tq?tqx=out:csv&sheet=Butterflies';
+const bfUrl = 'http://docs.google.com/spreadsheets/d/1BX7s2M5uOrcX0cgZmR7CnWcxh7U2YkRWE16d7TkwH5I/gviz/tq?tqx=out:csv&sheet=Butterflies';
 const outFile = 'ButterflyDatabase.js';
 
-request(url, { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  const bfdb = parse(body, { columns: true });
+updatedb = async () => {
+  const bfRes = await fetch(bfUrl);
+  const bfBody = await bfRes.text();
+
+  const bfdb = parse(bfBody, { columns: true });
   const bfdbString = `
     const imagelookup = {${bfdb.map(i => `'${i.image}': require('../assets/butterflies/${i.image}')`)}};
     const bfdb_raw = ${JSON.stringify(bfdb)};
@@ -23,4 +25,6 @@ request(url, { json: true }, (err, res, body) => {
     if (err) { return console.log(err); }
     console.log('Updated butterflies!');
   });
-});
+}
+
+updatedb();
