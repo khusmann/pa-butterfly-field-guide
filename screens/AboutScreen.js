3ae5bc897e9deb6f5d7b2c14992fa86df9bcf425
Meg Teuber <mrt226@psu.edu>
2019-05-08 11:38:00 -0400

    added what text

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -7,13 +7,13 @@ export default AboutScreen = (props) => {
       <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
         This app was designed by Margaret Teuber as part of her masters thesis on informal science learning and education at the Pennsylvania State University. She is a graduate assistant with Shaver's Creek Environmental Center.
       </Text>
-        <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
+      <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
         The goal of the research project is to study how people learn about biodiversity and the natural environment through their own initiative.
       </Text>
-        <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
+      <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
         The app was coded and created by Kyle Husmann. Without him, this project would not have been possible.
       </Text>
-        <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
+      <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
         The artwork used to represent each butterfly group was created by Bethany Gindhart.
       </Text>
     </ScrollView>
--- screens/WhatScreen.js
+++ screens/WhatScreen.js
@@ -5,7 +5,25 @@ export default WhatScreen = (props) => {
   return (
     <ScrollView style={styles.container}>
       <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
-        The 109 butterflies in this app are those...
+        The 109 butterflies in this app are those that are commonly found in Pennsylvania.
+      </Text>
+      <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
+        The butterflies are listed by their commonality. When looking at "All Butterflies" or looking at a butterfly group (subfamily), they are arranged from most common to least common.
+      </Text>
+      <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
+        In Pennsylvania, there are around 153 butterfly species that have been identified. Out of these 153 butterflies, 44 are not common and can look similar to common species.
+      </Text>
+      <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
+        If you do not see your butterfly:
+      </Text>
+      <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
+        1. Look again! Some butterfly species have some variation of appearance!
+      </Text>
+      <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
+        2. It may be a moth! Many moths look similar to butterflies, and many butterflies look like moths! The easiest way to tell them apart is that moths will have feathery antennae, while butterflies do not!
+      </Text>
+      <Text style={{ fontSize: 20, margin: 10, color: '#fff' }}>
+        3. If you double checked and it is definitely a butterfly, congratulations! You saw a rare butterfly! Rare butterflies are not necessarily endangered, it just means they are less common in Pennsylvania because of their natural range.
       </Text>
     </ScrollView>
   );
